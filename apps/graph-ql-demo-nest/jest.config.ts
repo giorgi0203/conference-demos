3ef2bb3b1a161d073b24cbc11dd014df70b85215
giorgi0203 <giorgi1997arabuli@gmail.com>
2023-07-16 12:40:55 +0000

    add nest project

---
+++ apps/graph-ql-demo-nest/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'graph-ql-demo-nest',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/graph-ql-demo-nest',
+};
